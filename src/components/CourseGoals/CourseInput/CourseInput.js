cee2a6738caae148cb31e51653966159ae1242dd
mriduladas <mridula199@gmail.com>
2022-04-14 17:10:24 +0530

    Using inline Style based on flag

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,39 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValidInput, setValidInput ] = useState(true);
+  let message;
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length ===0){
+      setValidInput(false);
+     
+    }else{
+      setValidInput(true);
+    }
+    
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length ===0){
+      setValidInput(false);
+      return
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValidInput? 'red': 'black' }}>Course Goal</label>
+        <input 
+        style={{backgroundColor: !isValidInput? 'red': '#ccc',
+                background:   !isValidInput? 'salmon': 'transparent'
+      }}
+        type="text" onChange={goalInputChangeHandler} />
+        
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
